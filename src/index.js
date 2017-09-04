f023929e7ed2bafed18d09118cd733690b7d582c
Pirkka Huhtala <pirkka.huhtala@gmail.com>
2017-09-04 15:28:29 +0300

    Fixing the layout and creating modules for scenes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './styles.css';
-import Layout from './components/layout/';
+import Layout from './components/layout';
+import StartUpScreen from './scenes/startupscreen';
+import GameBoard from './scenes/gameboard';
 
 ReactDOM.render (
-  <MuiThemeProvider><Layout /></MuiThemeProvider>,
+  <MuiThemeProvider>
+    <Layout />
+  </MuiThemeProvider>,
   document.getElementById ('root')
 );
